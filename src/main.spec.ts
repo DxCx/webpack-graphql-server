eaac74992450870389b437f65934d348a30e503e
Hagai Cohen <hagai.co@gmail.com>
2016-09-07 12:48:47 +0300

    Added tests for main as well

---
+++ src/main.spec.ts
@@ -0,0 +1,122 @@
+import { expect, should } from "chai";
+import { GRAPHQL_ROUTE, GRAPHIQL_ROUTE, main } from "./main";
+import { get as httpGet, Server } from "http";
+
+const ERRNO_KEY = "errno";
+const PORT: number = 8080;
+
+// Enable Chai's should
+should();
+
+function getFromServer(uri) {
+    return new Promise((resolve, reject) => {
+        httpGet(`http://localhost:${PORT}${uri}`, (res) => {
+            resolve(res);
+        }).on("error", (err: Error) => {
+            reject(err);
+        });
+    });
+}
+
+describe("main", () => {
+    it("should be able to Initialize a server (production)", () => {
+        return main({
+            enableCors: false,
+            enableGraphiql: false,
+            env: "production",
+            port: PORT,
+        }).then((server: Server) => {
+            return server.close();
+        });
+    });
+
+    it("should be able to Initialize a server (development)", () => {
+        return main({
+            enableCors: true,
+            enableGraphiql: true,
+            env: "dev",
+            port: PORT,
+        }).then((server: Server) => {
+            return server.close();
+        });
+    });
+
+    it("should have a working graphql (developemnt)", () => {
+        return main({
+            enableCors: true,
+            enableGraphiql: true,
+            env: "dev",
+            port: PORT,
+        }).then((server: Server) => {
+            return getFromServer(GRAPHQL_ROUTE).then((res: any) => {
+                server.close();
+                expect(res.statusCode).to.equal(405);
+            });
+        });
+    });
+
+    it("should have a working graphql (production)", () => {
+        return main({
+            enableCors: false,
+            enableGraphiql: false,
+            env: "production",
+            port: PORT,
+        }).then((server: Server) => {
+            return getFromServer(GRAPHQL_ROUTE).then((res: any) => {
+                server.close();
+                expect(res.statusCode).to.equal(405);
+            });
+        });
+    });
+
+    it("should have a working graphiql (developemnt)", () => {
+        return main({
+            enableCors: true,
+            enableGraphiql: true,
+            env: "dev",
+            port: PORT,
+        }).then((server: Server) => {
+            return getFromServer(GRAPHIQL_ROUTE).then((res: any) => {
+                server.close();
+                expect(res.statusCode).to.equal(200);
+            });
+        });
+    });
+
+    it("should have block graphiql (production)", () => {
+        return main({
+            enableCors: false,
+            enableGraphiql: false,
+            env: "production",
+            port: PORT,
+        }).then((server: Server) => {
+            return getFromServer(GRAPHIQL_ROUTE).then((res: any) => {
+                server.close();
+                expect(res.statusCode).to.equal(404);
+            });
+        });
+    });
+
+    it("should reject twice on same port", () => {
+        return main({
+            enableCors: false,
+            enableGraphiql: false,
+            env: "production",
+            port: PORT,
+        }).then((server: Server) => {
+            return main({
+                enableCors: false,
+                enableGraphiql: false,
+                env: "production",
+                port: PORT,
+            }).then((secondServer: Server) => {
+                server.close();
+                secondServer.close();
+                throw new Error("Was able to listen twice!");
+            }, (err: Error) => {
+                server.close();
+                expect(err[ERRNO_KEY]).to.equal("EADDRINUSE");
+            });
+        });
+    });
+});
--- src/main.ts
+++ src/main.ts
@@ -6,22 +6,31 @@ import * as cors from "cors";
 import * as helmet from "helmet";
 import * as morgan from "morgan";
 
-// Either to export GraphiQL (Debug Interface) or not.
-const NODE_ENV = process.env.NODE_ENV !== "production" ? "dev" : "production";
-const EXPORT_GRAPHIQL = NODE_ENV !== "production";
 // Default port or given one.
-const PORT = process.env.PORT || 3000;
-// Enable cors (cross-origin HTTP request) or not.
-const ENABLE_CORS = NODE_ENV !== "production";
+export const GRAPHQL_ROUTE = "/graphql";
+export const GRAPHIQL_ROUTE = "/graphiql";
 
-const GRAPHQL_ROUTE = "/graphql";
-const GRAPHIQL_ROUTE = "/graphiql";
+interface IMainOptions {
+    enableCors: boolean;
+    enableGraphiql: boolean;
+    env: string;
+    port: number;
+    verbose?: boolean;
+};
 
-function main() {
+/* istanbul ignore next: no need to test verbose print */
+function verbosePrint(port, enableGraphiql) {
+    console.log(`GraphQL Server is now running on http://localhost:${port}${GRAPHQL_ROUTE}`);
+    if ( true === enableGraphiql  ) {
+        console.log(`GraphiQL Server is now running on http://localhost:${port}${GRAPHIQL_ROUTE}`);
+    }
+}
+
+export function main(options: IMainOptions) {
     let app = express();
     app.use(helmet());
-    app.use(morgan(NODE_ENV));
-    if ( true === ENABLE_CORS ) {
+    app.use(morgan(options.env));
+    if ( true === options.enableCors ) {
         app.use(GRAPHQL_ROUTE, cors());
     }
 
@@ -29,20 +38,40 @@ function main() {
         context: {},
         schema: Schema,
     }));
-    if ( true === EXPORT_GRAPHIQL ) {
+    if ( true === options.enableGraphiql ) {
         app.use(GRAPHIQL_ROUTE, graphiqlExpress({
             endpointURL: GRAPHQL_ROUTE,
         }));
     }
-    app.listen(PORT, () => {
-        console.log(`GraphQL Server is now running on http://localhost:${PORT}${GRAPHQL_ROUTE}`);
-        if ( true === EXPORT_GRAPHIQL ) {
-            console.log(`GraphiQL Server is now running on http://localhost:${PORT}${GRAPHIQL_ROUTE}`);
-        }
+
+    return new Promise((resolve, reject) => {
+        let server = app.listen(options.port, () => {
+            /* istanbul ignore if: no need to test verbose print */
+            if ( options.verbose ) {
+                verbosePrint(options.port, options.enableGraphiql);
+            }
+
+            resolve(server);
+        }).on("error", (err: Error) => {
+            reject(err);
+        });
     });
 }
 
-/* istanbul ignore if: untestable */
+/* istanbul ignore if: main scope */
 if (require.main === module) {
-    main();
+    const PORT = process.env.PORT || 3000;
+    // Either to export GraphiQL (Debug Interface) or not.
+    const NODE_ENV = process.env.NODE_ENV !== "production" ? "dev" : "production";
+    const EXPORT_GRAPHIQL = NODE_ENV !== "production";
+    // Enable cors (cross-origin HTTP request) or not.
+    const ENABLE_CORS = NODE_ENV !== "production";
+
+    main({
+        enableCors: ENABLE_CORS,
+        enableGraphiql: EXPORT_GRAPHIQL,
+        env: NODE_ENV,
+        port: PORT,
+        verbose: true,
+    });
 }
